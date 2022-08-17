5672a8e4c449fa33196ae631e14ccd9fd2183018
micheal08 <mariamichealrajs@chevron.com>
2022-08-17 21:45:12 +0530

    New projects or updated

---
+++ dotnet/FileHelperSampleApp/FileHelperSampleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using FileHelpers;
+using FileHelpers.ExcelNPOIStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHelperSampleApp
+{
+    [DelimitedRecord("|")]
+    public class GenericLesson
+    {
+        #region Fields
+
+        public string FacilitySection;
+        public string Title;
+        public string Description;
+        public string PotentialCause;
+        public string Recommendation;
+        
+
+        #endregion Fields
+    }
+
+    [DelimitedRecord("|")]
+    public class VersionSheet
+    {
+        public string CurrentVersion;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericLesson[] genLess;
+            ExcelNPOIStorage provider = new ExcelNPOIStorage(typeof(GenericLesson))
+            {
+                SheetName = "LessonsLearned",
+                FileName = @"D:\Chevron Project Docs\Smart Engineering\LessonsLearned\testing_Template.xlsx",
+                StartRow = 1,
+                StartColumn = 0
+            };
+
+            genLess = (GenericLesson[])provider.ExtractRecords();
+
+            VersionSheet[] versionShee;
+            provider = new ExcelNPOIStorage(typeof(VersionSheet))
+            {
+                SheetName = "Version",
+                FileName = @"D:\Chevron Project Docs\Smart Engineering\LessonsLearned\testing_Template.xlsx",
+                StartRow = 1,
+                StartColumn = 0
+            };
+
+            versionShee = (VersionSheet[])provider.ExtractRecords();
+        }
+    }
+}
